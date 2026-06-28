eb98befaaee15a1ac0e264fc3109a3b2c4716526
Santino Maya <santinomaya60@gmail.com>
2026-06-28 17:41:56 -0300

    correccion color ruedita en LibroDetalle

--- ejercicios/c011-hooks/src/components/Descripcion.tsx
+++ ejercicios/c011-hooks/src/components/Descripcion.tsx
@@ -20,7 +20,7 @@ function Descripcion({libro_key}:DescripcionProps){
 
     if (loading) return (
         <div className="flex justify-center items-center h-40">
-            <div className="h-10 w-10 animate-spin rounded-full border-4 border-gray-300 border-t-blue-600"></div>
+            <div className="h-10 w-10 animate-spin rounded-full border-4 border-gray-300 border-t-red-600"></div>
         </div>
     )
 
